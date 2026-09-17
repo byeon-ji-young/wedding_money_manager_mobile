d6e892aa9cb0767b1db0494dfaa5464bafac740c
byeonjy <byeonjy8668@gmail.com>
2026-09-17 13:21:14 +0900

    refactor: 홈 화면 카테고리 관리 메뉴 제거

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,7 +8,6 @@ import '../models/settings.dart';
 import 'expense_register_screen.dart';
 import 'expense_list_screen.dart';
 import 'statistics_screen.dart';
-import 'category_management_screen.dart';
 import 'settings_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -206,19 +205,6 @@ class _HomeScreenState extends State<HomeScreen> {
             },
             icon: const Icon(Icons.bar_chart_rounded),
             tooltip: '지출 통계',
-          ),
-
-          IconButton(
-            onPressed: () async {
-              await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const CategoryManagementScreen(),
-                ),
-              );
-            },
-            icon: const Icon(Icons.category_rounded),
-            tooltip: '카테고리 관리',
           ),
 
           IconButton(
